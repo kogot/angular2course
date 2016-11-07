45967729400d2ce4848f421ce1abf5b2da01a198
Aleksei Kogtev <akogtev@gmail.com>
2016-11-08 00:17:22 +1000

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,22 @@ import {TaskComponent} from './task.component';
 @Component({
   selector: 'app-root',
   template: `<h1>{{title}}</h1>
+  <button class="btn btn-info" [class.active]="isActive"  
+  [style.backgroundColor]="isActive ? 'orange' : 'black'">Submit</button>
    <input type="text" autoGrow />
+   <div (click)="onDivClick()"><button (click)="onClick($event)">Click</button></div>
+   <button on-click="onClick()">Click #2</button>
+
+   <input type="text" [value]="title" (input)="title= $event.target.value" />
+   Preview: {{title}}
+   <button class="btn" (click)="title =''" value="Clear">Clear</button>
+
+<div>
+
+<input type="text" [(ngModel)]="title" />
+
+</div>
+
   <courses></courses>
   <tasks></tasks>
   `,
@@ -14,4 +29,14 @@ import {TaskComponent} from './task.component';
 })
 export class AppComponent {
   title = 'app works!';
+  isActive =true;
+
+  onClick($event){
+    $event.stopPropagation();
+    console.log("click", $event);
+  }
+
+  onDivClick(){
+    console.log("handled by div")
+  }
 }
